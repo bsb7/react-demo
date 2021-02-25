5aa699aa3cc844c1464563045ff23646887f32b7
bsb7 <breboneriabernadette@gmail.com>
2021-02-26 03:22:57 +0800

    adding Person component

---
+++ src/Person/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const person = () => {
+   return <p>I'm a person</p> 
+}
+
+export default person
+
+
+
+
+
+
+
+
+
+
+// Component - in it's simplest form is just a function which returns some jsx, some html
