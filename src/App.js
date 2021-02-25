5c334d381034c58b4c751c0ef9d27159791f6553
bsb7 <breboneriabernadette@gmail.com>
2021-02-26 03:00:01 +0800

    react basic

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hi, I'm a React App!</h1>
+    </div>
+  );
+}
+
+export default App;
+
+
+// react - has one method, the render method
+//         react will call this method to render something to the screen 
+//         every react component has to render some html code which can be rendered to the dom to the screen, can do other things but the basic is to return some html code
